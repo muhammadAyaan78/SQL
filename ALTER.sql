46c98bf21482e426e7b18a3c0abd07416699fe49
muhammadAyaan78 <muhammadayaanshaukat@gmail.com>
2026-01-20 21:35:25 +0500

    Add files via upload

---
+++ ALTER.sql
@@ -0,0 +1,10 @@
+ALTER TABLE person2
+--	ADD gmail VARCHAR(50) NOT NULL
+DROP COLUMN birth_date 
+
+
+SELECT *
+FROM person2
+-- Now if you want to remove any particular table from the column we will use the clause of DROP
+SELECT * FROM orders
+UPDATE TABLE
