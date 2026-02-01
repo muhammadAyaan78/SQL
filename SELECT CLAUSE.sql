c313dd0f76b4cdb7db237a84f766abd7f3196259
Muhammad Ayaan Shaukat <muhammadayaanshaukat@gmail.com>
2026-02-01 14:58:03 +0500

    Add files via upload

---
+++ SELECT CLAUSE.sql
@@ -0,0 +1,12 @@
+SELECT * FROM agriculture_dataset
+
+-- * means select every column in our desired table
+-- SELECT (COLUMN-NAME) FROM (TABLE)
+-- ORDER OF EXECUTION: 1) FROM 2) SELECT
+-- From this particular table select this particular column
+
+SELECT Farm_Area_acres,Farm_ID From agriculture_dataset
+select * from agriculture_dataset
+-- if we want to execute one statement from a 1000 statement sql file then we will highlight
+--that particular statement and execute it.
+-- SQL syntax wo case sensitive nhi hai
